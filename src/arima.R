f33ced4e589a2069cda95a214036b14f5649b6d0
SaBart <samuell.bartos@gmail.com>
2017-02-26 20:38:06 +0100

    fourier coefficients

--- src/arima.R
+++ src/arima.R
@@ -7,31 +7,35 @@ pop_col=function(data,col){ # removes and returns column from dataframe
   return(poped_col)
 }
 
+f_ords<-function(train,freq=24,freqs,max_order){
+  params<-expand.grid(lapply(freqs,function(x) seq(max_order))) # all combinations of fourier orders
+  aicc_best<-Inf # best aicc statistic
+  param_best<-NULL # best parameters
+  for (i in 1:nrow(params)){ # for each combination of orders
+    param<-unlist(params[i,]) # combination of orders
+    xreg_train<-fourier(msts(train,seasonal.periods=freqs),K=param) # fourier terms for particular multi-seasonal time series
+    fit=auto.arima(ts(c(t(train)),frequency = freq),xreg=xreg_train,seasonal=FALSE,parallel = TRUE,stepwise=FALSE) # find best arima model
+    if (fit$aicc<aicc_best){ # if there is an improvement in aicc statistic
+      param_best<-param # save these orders
+      aicc_best<-fit$aicc # save new best aicc value
+    }
+    print(param)
+    print(fit$aicc)
+  }
+  return(param_best)
+}
 
-arima<-function(train,test,hor=1,batch=7,freq=7,xreg_train=NULL,xreg_test=NULL,freqs=NULL){
-  if (is.null(freqs)){ # not considering multiple seasonalities
-    dummy_year_train=model.matrix(~as.factor(year(rownames(train))))[,1:11]
-    
-    dummy_year=model.matrix(~as.factor(year(c(rownames(train),rownames(test)))))
-    dummy_year_test=model.matrix(~as.factor(year(rownames(test))))[,1:11]
+
+arima<-function(train,test,hor=1,batch=7,freq=24,NULL,fourier=NULL,xreg_train=NULL,xreg_test=NULL){
+  pb<-txtProgressBar(min = 0, max = nrow(test), style = 3) # initialize progress bar
+  if (is.null(fourier)){ # not considering multiple seasonalities
+    xreg_train<-NULL
   }
   else { # considering multiple seasonalities
-    params=expand.grid(lapply(K,function(x) seq(0,x%/%2))) # all combinations of fourier orders
-    aicc_best=Inf
-    param_best
-    for (i in 1:nrow(params)){ # for each combination of orders
-      param=unlist(params[i,] # order combination
-      xreg_train=fourier(msts(train,seasonal.periods=freqs),K=param) # 
-      fit=auto.arima(ts(c(t(train)),frequency = freq),xreg=xreg_train)
-      if (fit$aicc<aicc_best){
-        param_best=param
-        aicc_best=fit$aicc
-        }
-    }
-    
+    xreg_train<-fourier(msts(train,seasonal.periods=freqs),K=fourier)
   }
   test_pred<-data.frame(matrix(data=NA,nrow=nrow(test),ncol=ncol(test),dimnames=list(rownames(test),colnames(test)))) # initialize matrix for predictions
-  pb<-txtProgressBar(min = 0, max = nrow(test), style = 3) # initialize progress bar
+ 
   for (i in 0:nrow(test)){ # for each sample in test set
     test_ts<-ts(c(t(rbind(train,head(test,i)))),frequency=freq) # add a new day from test set to the current train set
     if (is.null(xreg)){ # not considering external regressors
@@ -53,11 +57,11 @@ arima<-function(train,test,hor=1,batch=7,freq=7,xreg_train=NULL,xreg_test=NULL,f
   return(data.frame(test_pred))
 }
 
-arima_h<-function(train,test,batch=7,freq=24){
+arima_h<-function(train,test,batch=7,freq=24,freqs=NULL,fourier=NULL,xreg_train=NULL,xreg_test=NULL){
   return(arima(train,test,hor=24,batch=batch,freq=freq))
 }
 
-arima_v<-function(train,test,batch=7,freq=7){
+arima_v<-function(train,test,batch=7,freq=7,freqs=NULL,fourier=NULL,xreg_train=NULL,xreg_test=NULL){
   test_pred<-as.data.frame(lapply(test, function(x) rep.int(NA, length(x)))) # template dataframe for predictions
   for (col in names(train)){
     train_day<-as.data.frame(train[[col]]) # convert dataframe column to dataframe
@@ -71,8 +75,7 @@ arima_v<-function(train,test,batch=7,freq=7){
 
 dir<-'C:/Users/SABA/Google Drive/mtsg/data/' # directory containing data
 
-train<-read.csv(paste(dir,'train.csv', sep=''),header=TRUE,row.names='date',sep=',',dec='.') # load train set
-test<-read.csv(paste(dir,'test.csv', sep=''),header=TRUE,row.names='date',sep=',',dec='.') # load test set
+# NO EXTERNAL REGRESSORS
 
 # horizontal predictions
 test_pred_h<-arima_h(train,test,batch=28,freq=24) # horizontal prediction
@@ -99,6 +102,23 @@ for (i in 0:6){ # for each day
   test_pred_vw<-arima_v(train,test,batch=4,freq=4) # horizontal predictions for this day
   write.csv(test_pred_vw,file=paste(dir,'arima_rv_',i,'.csv',sep='')) # write results
 }
+
+# FOURIER EXTERNAL REGRESSORS
+
+train<-read.csv(paste(dir,'train.csv', sep=''),header=TRUE,row.names='date',sep=',',dec='.') # load train set
+test<-read.csv(paste(dir,'test.csv', sep=''),header=TRUE,row.names='date',sep=',',dec='.') # load test set
+
+# horizontal predictions
+K<-f_ords(train,freq=24,freqs=c(24*7,365.25*7),max_order=10) # find best fourier coefficients
+test_pred_h<-arima_h(train,test,batch=28,freq=24,freqs=c(24*7,365.25*7),fourier=K) # horizontal prediction
+rownames(test_pred_h)<-date_test # set "index"
+write.csv(test_pred_h,file<-'C:/Users/SABA/Google Drive/mtsg/code/load_forecast/data/arima_rh.csv',quote = FALSE) # write predictions
+
+
+# WEATHER EXTERNAL REGRESSORS
+
+
+# FOURIER & WEATHER EXTERNAL REGRESSORS
 
 
 
